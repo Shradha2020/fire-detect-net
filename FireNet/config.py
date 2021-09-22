9c659994d85078fe1b043a6e30d150b57fe0da33
Bijayani Mishra <38001077+bijayaniSMishra@users.noreply.github.com>
2021-09-21 17:42:21 -0700

    Update config.py

--- FireNet/config.py
+++ FireNet/config.py
@@ -1,23 +1,9 @@
 from detectron2.config import CfgNode as CN
 
+from detectron2.config import CfgNode as CN
+from detectron2.projects.panoptic_deeplab import add_panoptic_deeplab_config
+
+
 
 def add_firenet_config(cfg):
-    """
-    Add config for firenet.
-    """
-    _C = cfg
-
-    _C.MODEL.FIRENET = CN()
-
-    # Number of branches for FIRENET.
-    _C.MODEL.FIRENET.NUM_BRANCH = 3
-    # Specify the dilations for each branch.
-    _C.MODEL.FIRENET.BRANCH_DILATIONS = [1, 2, 3]
-    # Specify the stage for applying FIRENET blocks. Default stage is Res4 according to the
-    # TridentNet paper.
-    _C.MODEL.FIRENET.TRIDENT_STAGE = "res4" # Need to check if needed.
-    # Specify the test branch index FIRENET Fast inference:
-    #   - use -1 to aggregate results of all branches during inference.
-    #   - otherwise, only using specified branch for fast inference. Recommended setting is
-    #     to use the middle branch.
-    _C.MODEL.FIRENET.TEST_BRANCH_IDX = 1
+    add_panoptic_deeplab_config(cfg)
