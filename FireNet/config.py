0dd6d03f6be4cafb6d0139e58c546a168d481562
Shradha Shaw <71346494+Shradha2020@users.noreply.github.com>
2021-09-21 20:22:53 -0700

    Update config.py

--- FireNet/config.py
+++ FireNet/config.py
@@ -1,9 +1,5 @@
 from detectron2.config import CfgNode as CN
-
-from detectron2.config import CfgNode as CN
 from detectron2.projects.panoptic_deeplab import add_panoptic_deeplab_config
-
-
 
 def add_firenet_config(cfg):
     add_panoptic_deeplab_config(cfg)
